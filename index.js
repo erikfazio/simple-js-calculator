cd6238958e7cb6ae81fb0d83ff837cfe098169fb
erikfazio <fazioerik@gmail.com>
2023-09-09 07:55:00 +0200

    Refactoring code

--- index.js
+++ index.js
@@ -1,70 +1,76 @@
-let input = "";
-let result = "";
+let firstOperand = "";
+let secondOperand = "";
 let currentOperator = "";
 
-const calcButtonList = document.querySelectorAll(".calc-button");
-const inputElement = document.querySelector(".screen");
+const screen = document.querySelector(".screen");
 
-function saveInput(text) {
-  input += text;
-  inputElement.value += text;
+function updateScreen(value) {
+  input += value;
+  screen.value += value;
 }
 
-function operation(a, b, operator) {
-  console.log(a, b, operator);
+function clearScreen() {
+  input = 0;
+  screen.value = "";
+  currentOperator = "";
+}
+
+function calculate(a, b, operator) {
   if (operator === "+") {
     return a + b;
   } else if (operator === "-") {
     return a - b;
-  } else if (operator === "x") {
+  } else if (operator === "*") {
     return a * b;
   } else if (operator === "/") {
     return a / b;
   } else return 0;
 }
 
-function clear() {
-  input = 0;
-  inputElement.value = "";
-  currentOperator = "";
-}
-
-function updateScreen(text) {
-  console.log(text);
-  if (
-    text === "1" ||
-    text === "2" ||
-    text === "3" ||
-    text === "4" ||
-    text === "5" ||
-    text === "6" ||
-    text === "7" ||
-    text === "8" ||
-    text === "9" ||
-    text === "0"
-  ) {
-    inputElement.value += text;
-    if (currentOperator !== "") {
-      result += text;
-    }
-  } else if (text === "+" || text === "x" || text === "-" || text === "/") {
-    input = inputElement.value;
-    currentOperator = text;
-    inputElement.value = "";
-  } else if (text === "C") {
-    clear();
-  } else if (text === "=") {
-    const newValue = operation(Number(input), Number(result), currentOperator);
-    input = newValue;
-    inputElement.value = newValue;
-    result = "";
+function handleSymbol(value) {
+  if (value === "+" || value === "*" || value === "-" || value === "/") {
+    currentOperator = value;
+    screen.value = "";
+  } else if (value === "C") {
+    clearScreen();
+  } else if (value === "=") {
+    const newValue = calculate(
+      Number(firstOperand),
+      Number(secondOperand),
+      currentOperator
+    );
+    firstOperand = newValue;
+    screen.value = newValue;
+    secondOperand = "";
     currentOperator = "";
   }
 }
 
-for (let index = 0; index < calcButtonList.length; index++) {
-  const element = calcButtonList[index];
-  element.addEventListener("click", (event) =>
-    updateScreen(event.target.innerHTML)
-  );
+function handleNumber(value) {
+  if (currentOperator === "") {
+    firstOperand += value;
+  } else {
+    secondOperand += value;
+  }
+  screen.value += value;
 }
+
+function buttonClick(value) {
+  if (isNaN(parseInt(value))) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+}
+
+function init() {
+  const calcButtonList = document.querySelectorAll(".calc-button");
+
+  calcButtonList.forEach(function (calcButton) {
+    calcButton.addEventListener("click", function (event) {
+      buttonClick(event.target.innerText);
+    });
+  });
+}
+
+init();
